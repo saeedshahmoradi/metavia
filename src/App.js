dcae9917b786ba3bf58115117c103f80c9811346
Afshin Shah <BeatificKing@users.noreply.github.com>
2024-12-07 19:48:11 +0330

    animated background added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import DesktopNavbar from './components/desktopNavbar/DesktopNavbar';
 import routes from './routes';
 import { AnimatePresence, motion } from 'framer-motion';
 import FixedArea from './components/fixedArea/FixedArea';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
 
   const location = useLocation();
   const router = useRoutes(routes);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    const { clientX, clientY, currentTarget } = event;
+
+    const { width, height, left, top } = currentTarget.getBoundingClientRect();
+    const centerX = left + width / 2;
+    const centerY = top + height / 2;
+
+    const offsetX = (clientX - centerX) / (width / 2);
+    const offsetY = (clientY - centerY) / (height / 2);
+
+    setOffset({
+      x: offsetX * 10,
+      y: offsetY * 10,
+    });
+  };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container}
+      onMouseMove={handleMouseMove}
+      style={{ backgroundPosition: `${50 + offset.x}% ${50 + offset.y}%` }}
+    >
       <main className={styles.main}>
         <FixedArea />
         <section className={styles.variable_area}>
--- src/components/fixedArea/FixedArea.jsx
+++ src/components/fixedArea/FixedArea.jsx
@@ -1,8 +1,11 @@
 import { memo } from 'react';
 import styles from './fixedArea.module.css';
-import { PiInstagramLogoLight } from "react-icons/pi";
-import { PiTelegramLogoLight } from "react-icons/pi";
-import { PiWhatsappLogoLight } from "react-icons/pi";
+// import { PiInstagramLogoLight } from "react-icons/pi";
+// import { PiTelegramLogoLight } from "react-icons/pi";
+// import { PiWhatsappLogoLight } from "react-icons/pi";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTelegramPlane } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const FixedArea = memo(() => {
   return (
@@ -11,9 +14,12 @@ const FixedArea = memo(() => {
       <h1 className={styles.groupName}>Dev Group</h1>
       <strong className={styles.shortDescription}>Web and App Design</strong>
       <div className={styles.social}>
-        <PiInstagramLogoLight className={styles.social_icon} />
+        {/* <PiInstagramLogoLight className={styles.social_icon} />
         <PiTelegramLogoLight className={styles.social_icon} />
-        <PiWhatsappLogoLight className={styles.social_icon} />
+        <PiWhatsappLogoLight className={styles.social_icon} /> */}
+        <AiFillInstagram className={styles.social_icon} />
+        <FaTelegramPlane className={styles.social_icon} />
+        <IoLogoWhatsapp className={styles.social_icon} />
       </div>
     </section>
   )
